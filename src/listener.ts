ec3c3038dcccfd26e4422f3018fba3711972f754
adc work <adc9work@outlook.com>
2020-09-07 11:56:59 +0530

    Durable Name

--- src/listener.ts
+++ src/listener.ts
@@ -1,19 +1,39 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 console.clear();
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listner connected to NATS");
 
-  const subsrciption = stan.subscribe("ticket:created");
+  stan.on("close", () => {
+    console.log("NATS Connection Closed!");
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("accounting-service");
+
+  const subsrciption = stan.subscribe(
+    "ticket:created",
+    "queue-group-name",
+    options
+  );
   subsrciption.on("message", (msg: Message) => {
     console.log("Message Received");
     const data = msg.getData();
     if (typeof data === "string") {
       console.log(`Received event #${msg.getSequence()}, with data:${data}`);
     }
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
