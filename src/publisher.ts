f5aa3de1f9f4c1f66dce57670e3649028e4ee870
adc work <adc9work@outlook.com>
2020-09-17 15:40:45 +0530

    Done

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -6,17 +7,27 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
-  console.log("publisher connected to Nats");
+stan.on("connect", async () => {
+  console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
-  //Pubish Method
-  stan.publish("ticket:created", data, () => {
-    console.log("Event Published");
-  });
+  // const data = JSON.stringify({
+  //   id: "123",
+  //   title: "concert",
+  //   price: 20,
+  // });
+
+  // stan.publish("ticket:created", data, () => {
+  //   console.log("Event published");
+  // });
 });
